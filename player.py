453aaf480f3543b6ca778d73995197ca69c4fdf5
Curtis Chong <curtischong5@gmail.com>
2024-12-20 11:51:50 -0800

    reordering and combination action masks

--- player.py
+++ player.py
@@ -19,12 +19,10 @@ class Player:
         return player
 
     # returns if this reorder is a valid move
-    def reorder_team(self, pet_start_idx: int, pet_end_idx: int) -> bool:
+    def reorder_team_action(self, pet_start_idx: int, pet_end_idx: int):
         pets = self.team.pets
-        if pets[pet_start_idx].species == Species.NONE:
-            return False
-        if pet_start_idx == pet_end_idx:
-            return False
+        assert pets[pet_start_idx].species != Species.NONE
+        assert pet_start_idx != pet_end_idx
 
         # Remove the pet from the old position
         pet_to_move = pets.pop(pet_start_idx)
@@ -34,15 +32,33 @@ class Player:
 
         return True
 
+    def reorder_team_action_mask(self) -> np.ndarray:
+        mask = np.ones((MAX_TEAM_SIZE, MAX_TEAM_SIZE), dtype=np.bool)
+
+        # cannot use itertools.combinations since reordering does NOT commute (who is the first pet matters)
+        for pet_start_idx in range(MAX_TEAM_SIZE):
+            for pet_end_idx in range(MAX_TEAM_SIZE):
+                # you cannot move a pet to the same spot
+                if pet_start_idx == pet_end_idx:
+                    mask[pet_start_idx, pet_end_idx] = False
+                    continue
+
+                # you cannot move an empty pet
+                pet = self.team.pets[pet_start_idx]
+                if pet.species == Species.NONE:
+                    mask[pet_start_idx, pet_end_idx] = False
+        return mask
+
     # TODO: do we allow combining pets if the target pet is already at max level?
     # drag pet 1 to pet 2
     def combine_pets_action(self, pet1_idx: int, pet2_idx: int) -> bool:
-        assert pet1_idx == pet2_idx
+        assert pet1_idx != pet2_idx
 
         pet1 = self.team.pets[pet1_idx]
         pet2 = self.team.pets[pet2_idx]
 
-        assert pet1.species != pet2.species
+        assert pet1.species == pet2.species
+        assert pet1.species != Species.NONE
 
         new_pet = pet1.combine_onto(pet2)
         self.team.pets[pet2_idx] = new_pet
@@ -53,6 +69,7 @@ class Player:
         # the mask is NOT of size n choose 2 since the order of the merged pet matters (dictates who we're merging ONTO).
         mask = np.ones((MAX_TEAM_SIZE, MAX_TEAM_SIZE), dtype=np.bool)
 
+        # we can use itertools since combine_pets validity commutes
         for pet1_idx, pet2_idx in itertools.combinations(range(MAX_TEAM_SIZE), 2):
             # you cannot combine a pet with itself
             if pet1_idx == pet2_idx:
@@ -65,6 +82,14 @@ class Player:
             if pet1.species != pet2.species:
                 mask[pet1_idx, pet2_idx] = False
                 mask[pet2_idx, pet1_idx] = False
+                continue
+
+            # you cannot combine an empty pet
+            if pet1.species == Species.NONE:
+                mask[pet1_idx, pet2_idx] = False
+                mask[pet2_idx, pet1_idx] = (
+                    False  # we can make the same relation since we know the species are the same
+                )
         return mask
 
     def buy_pet_at_slot(self, slot_idx: int, target_team_idx: int) -> bool:
