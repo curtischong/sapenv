5ca468bec511b2760c3720af99bf7050270001c3
Curtis Chong <curtischong5@gmail.com>
2024-12-20 14:54:46 -0800

    step function now handles when the game ends

--- environment/environment.py
+++ environment/environment.py
@@ -1,4 +1,5 @@
 import gymnasium as gym
+from all_types_and_consts import GameResult
 from environment.state_space import (
     env_observation_space,
     get_observation,
@@ -23,9 +24,20 @@ class SuperAutoPetsEnv(gym.Env):
     def step(self, action):
         # Implement your logic for applying the action and transitioning to the next state
         observation = get_observation(self.player)  # Implement this method
-        reward = 0.0
-        done = False
-        info = {}
+        game_result = (
+            self.player.end_turn_action()
+        )  # Get the game result after the action
+
+        # Determine if the game is done based on the result
+        info = {"game_result": game_result}
+        reward = 0
+        if game_result == GameResult.WIN:
+            reward = 100
+            done = True
+        elif game_result == GameResult.LOSE:
+            reward = -100 + self.player.num_wins * 10
+            done = True
+
         return observation, reward, done, info
 
     def render(self):
