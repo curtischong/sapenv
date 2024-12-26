7440797632be047a0745b77146d9ea3bcf65fe50
Curtis Chong <curtischong5@gmail.com>
2024-12-26 15:59:50 -0500

    harsh penalty for truncating

--- environment/environment.py
+++ environment/environment.py
@@ -89,8 +89,9 @@ class SuperAutoPetsEnv(gym.Env):
             truncated = True
             self.reset()
             info = {}
-            if reward > 0:
-                reward = reward / 2
+            # if reward > 0:
+            #     reward = reward / 2
+            reward = -200  # have a harsh penalty for truncating. since otherwise, it just stalls to prevent getting losing penalties
             if self.wandb_run:
                 self.wandb_run.log(
                     {
