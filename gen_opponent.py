4a8c45425c6e959cf1103c90884fd142da781b47
Curtis Chong <curtischong5@gmail.com>
2024-12-26 11:59:32 -0500

    make opponent beatable

--- gen_opponent.py
+++ gen_opponent.py
@@ -26,15 +26,9 @@ def get_horse_team(round_number: int):
     additional_stats = round_number - 1
     return Team(
         [
-            get_base_pet(Species.HORSE).add_stats(
-                attack=additional_stats, health=additional_stats
-            ),
-            get_base_pet(Species.HORSE).add_stats(
-                attack=additional_stats, health=additional_stats
-            ),
-            get_base_pet(Species.HORSE).add_stats(
-                attack=additional_stats, health=additional_stats
-            ),
+            get_base_pet(Species.HORSE).add_stats(attack=0, health=additional_stats),
+            get_base_pet(Species.HORSE),
+            get_base_pet(Species.HORSE),
             get_base_pet(Species.NONE),
             get_base_pet(Species.NONE),
         ]
